dbc685063460cc01e5e4d932c5476d80743e0a0b
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-07 12:28:35 +0900

    スクロール出来るようにした兼コミット練習

--- UserManagementApp/ViewController.swift
+++ UserManagementApp/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var scroller: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
