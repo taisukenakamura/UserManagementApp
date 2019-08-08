23c32631bf8e991e9ccf673a28b0344f2e7b4bb4
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-08 09:25:27 +0900

    戻るボタンの実装、コメント入力

--- UserManagementApp/ViewController.swift
+++ UserManagementApp/ViewController.swift
@@ -19,30 +19,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let backButtonItem = UIBarButtonItem(title: "戻る", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backButtonItem
     }
     
-    func checkName(){
-        
-    }
     
-    
+//    全てのボタンが押された後の処理
     @IBAction func tappedButton(_ sender: UIButton) {
+//        tag番号の情報全てを一時的にsendInformationに格納
         let sendInformation = sender.tag
+//        最初に初期化して定義した変数に代入
         buttonTag = sendInformation
+//        ボタンが押された際の遷移先に対する処理の実行
         performSegue(withIdentifier: "function", sender: nil)
     }
     
     
     
     
-    
+//    画面遷移前の処理
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+//        mainstoryboradで定義したsegueのidentifierであるかどうか
+//        遷移先がSVCであること
     guard segue.identifier == "function", let svc = segue.destination as?
         SecondViewController else {
             return
             
         }
+//        guard文の中身が確認された後に、buttonTagに代入したボタンのタグ情報をSVCでも使用できるようにする
         svc.getTag = buttonTag
         }
 //
